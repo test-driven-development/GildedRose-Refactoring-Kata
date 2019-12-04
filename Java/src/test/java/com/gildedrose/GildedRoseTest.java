100bf1f72cafe478ff7db4035c79136cfb4fc22d
wpannell <wil.pannell@gmail.com>
2019-12-04 06:09:41 -0500

    swap out junit 5 and swap in junit 4

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,16 +1,21 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.approvaltests.Approvals;
+import org.approvaltests.reporters.JunitReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-class GildedRoseTest {
+@UseReporter(JunitReporter.class)
+public class GildedRoseTest {
 
   @Test
-  void updateQuality() {
+  public   void updateQuality() {
     Item[] items = new Item[] {new Item("foo", 0, 0)};
     GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertEquals("foo", app.items[0].name);
+    Approvals.verify(app.items[0].name);
   }
 }
